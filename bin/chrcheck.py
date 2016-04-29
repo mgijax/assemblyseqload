6821c6dc04b9b204e8a785ade74e36ebd7d60948
Lori Corbani <lori.corbani@jax.org>
2016-04-29 07:33:49 -0400

    TR12069/postgres cleanup

--- bin/chrcheck.py
+++ bin/chrcheck.py
@@ -20,6 +20,9 @@ import os
 import string
 import db
 
+db.setAutoTranslate(False)
+db.setAutoTranslateBE(False)
+
 TAB = '\t'
 NL = '\n'
 
@@ -34,39 +37,36 @@ reportFile = open(reportFileName, 'w')
 db.useOneConnection(1)
 
 # Resolve logical db name to key
-results = db.sql('select _LogicalDB_key ' + \
-	'from ACC_LogicalDB ' + \
-	'where name = "%s"' % logicalDB, 'auto')
+results = db.sql('''select _LogicalDB_key from ACC_LogicalDB where name = '%s' ''' % logicalDB, 'auto')
 ldbKey = results[0]['_LogicalDB_key']
 if ldbKey == '':
     print 'Invalid Coordinate LogicalDB name'
     exit(1)
 
 # Resolve collection name to key
-results = db.sql('select _Collection_key ' + \
-        'from MAP_Coord_Collection ' + \
-        'where name = "%s"' % collection, 'auto')
+results = db.sql('''select _Collection_key from MAP_Coord_Collection where name = '%s' ''' % collection, 'auto')
 collKey = results[0]['_Collection_key']
 if collKey == '':
     print 'Invalid Coordinate Collection name'
     exit(1)
 
 # query for inconsistencies
-results = db.sql('select a1.accID, m.symbol, cc.chromosome as geneModelChr, m.chromosome as markerChr ' + \
-    'from MAP_Coordinate c, MRK_Chromosome cc, MAP_Coord_Feature f, ' + \
-    'ACC_Accession a1, ACC_Accession a2, MRK_Marker m ' + \
-    'where c._Collection_key = %s ' % collKey + \
-    'and c._MGIType_key = 27 ' + \
-    'and c._Object_key = cc._Chromosome_key ' + \
-    'and c._Map_key = f._Map_key ' + \
-    'and f._Object_key = a1._Object_key ' + \
-    'and a1._MGIType_key = 19 ' + \
-    'and a1._LogicalDB_key = %s ' % ldbKey + \
-    'and a1.accID = a2.accID ' + \
-    'and a2._MGIType_key = 2 ' + \
-    'and a2._LogicalDB_key = %s ' % ldbKey + \
-    'and a2._Object_key = m._Marker_key  ' + \
-    'and cc.chromosome != m.chromosome ', 'auto')
+results = db.sql('''select a1.accID, m.symbol, cc.chromosome as geneModelChr, m.chromosome as markerChr
+    from MAP_Coordinate c, MRK_Chromosome cc, MAP_Coord_Feature f, 
+         ACC_Accession a1, ACC_Accession a2, MRK_Marker m 
+    where c._Collection_key = %s 
+    and c._MGIType_key = 27 
+    and c._Object_key = cc._Chromosome_key 
+    and c._Map_key = f._Map_key 
+    and f._Object_key = a1._Object_key 
+    and a1._MGIType_key = 19 
+    and a1._LogicalDB_key = %s 
+    and a1.accID = a2.accID 
+    and a2._MGIType_key = 2 
+    and a2._LogicalDB_key = %s 
+    and a2._Object_key = m._Marker_key 
+    and cc.chromosome != m.chromosome 
+    ''' % (collKey, ldbKey, ldbKey), 'auto')
 
 reportFile.write('%s with different Chromosome that its associated Marker%s%s' % (logicalDB, NL, NL))
 reportFile.write('Gene Model ID%sSymbol%sGene Model Chr%sMarker Chr%s' % (TAB, TAB, TAB, NL))
